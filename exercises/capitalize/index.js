839f885d898fb7cdf51f66a134310bf8c2e58f45
Vinhlee95 <lethanhvinh95@gmail.com>
2018-02-08 01:15:07 +0200

    added 2nd solution for capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,14 +8,29 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-   //split str to an array of words
-   const arr = str.split(" ");
-   let results = [];
-   //For each word inside the array, capitalize the first letter
-   for(let word of arr) {
-      results.push(word[0].toUpperCase() + word.slice(1));
+   //store the first char of the str in a variable
+   let result = str[0].toUpperCase();
+   //loop thru the rest of the str
+   for(var i = 1; i < str.length; i++) {
+      //condition the left of a char in the str
+      if(str[i-1] === ' ') {
+         result += str[i].toUpperCase();
+      } else {
+         result += str[i];
+      }
    }
-   return results.join(' ');
+   return result;
 }
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//   //split str to an array of words
+//   const arr = str.split(" ");
+//   let results = [];
+//   //For each word inside the array, capitalize the first letter
+//   for (let word of arr) {
+//     results.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return results.join(" ");
+// }
